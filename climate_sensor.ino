d3cc71a716a8d66a8717a9e4fdfb36b963042660
yarno97x <127676205+yarno97x@users.noreply.github.com>
2023-04-01 16:23:21 -0400

    Added comments for clarity

--- climate_sensor.ino
+++ climate_sensor.ino
@@ -1,9 +1,9 @@
-#include <SD.h>
-#include <DHT.h>
-#include <Wire.h>
+#include <SD.h> // SD library for file reading and modifications 
+#include <DHT.h> // DHT library for climate sensor
+#include <Wire.h> // Wire Library for I2C module configuration
 
 #define DHTPIN 4 // connect DHT11 sensor to I/O pin 4 on I2C module
-#define DHTTYPE DHT11
+#define DHTTYPE DHT11 // specify DHT sensor type
 #define SD_CS 7 // chip select pin for SD card module
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -16,32 +16,32 @@ void setup() {
     return;
   }
   Serial.println("SD card initialized.");
-  dht.begin();
+  dht.begin(); 
 }
 
 void loop() {
-  float humidity = dht.readHumidity();
+  float humidity = dht.readHumidity(); // read values 
   float temperature = dht.readTemperature();
-  Serial.print("Humidity: ");
+  Serial.print("Humidity: "); // print values in terminal
   Serial.print(humidity);
   Serial.print("% - Temperature: ");
   Serial.print(temperature);
   Serial.println("C");
 
-  String fileName = "Readings.txt";
-  File dataFile = SD.open(fileName, FILE_WRITE);
+  String fileName = "Readings.txt"; // name the file "Readings.txt"
+  File dataFile = SD.open(fileName, FILE_WRITE); // open file named "Readings.txt" on SD card
   if (dataFile) {
-    dataFile.print("Humidity: ");
+    dataFile.print("Humidity: "); // print values to SD card
     dataFile.print(humidity);
     dataFile.print("% - Temperature: ");
     dataFile.print(temperature);
     dataFile.println("C");
-    dataFile.close();
+    dataFile.close(); // close the file
   }
   else {
-    Serial.println("Error opening file.");
+    Serial.println("Error opening file."); // print error message if file is missing
   }
 
-  delay(10000);
+  delay(10000); // take readings every 10 seconds
 }
 
